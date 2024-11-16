09c96ae2645a647f7994ba1ffecae6a715c102e7
Carly Matson <carlymatson21@gmail.com>
2024-11-16 10:45:20 -0600

    Makes 2023/12/1 typer-friendly

--- aoc/y2023/day01/solve.py
+++ aoc/y2023/day01/solve.py
@@ -1,11 +1,10 @@
 """Day 1: Trebuchet?!
+
 https://adventofcode.com/2023/day/1
 """
 
 import re
 from pathlib import Path
-
-input_file = Path(__file__).parent / "input.txt"
 
 digit_mappings = {
     "zero": 0,
@@ -19,6 +18,7 @@ digit_mappings = {
     "eight": 8,
     "nine": 9,
 }
+digit_pattern = "|".join([r"\d", *digit_mappings.keys()])
 
 
 class Solver:
@@ -29,51 +29,55 @@ class Solver:
     def parse_input(self, raw_input: str) -> list[str]:
         return raw_input.split("\n")
 
-    def get_calibration_value(self, line: str) -> int:
-        """Search for first and last numeral 0-9."""
-        digits = re.findall(r"\d", line)
-        first, last = (digits[0], digits[-1])
-        return 10 * int(first) + int(last)
+    @staticmethod
+    def find_numeral_digits(line: str) -> list[int]:
+        return [int(match) for match in re.findall(r"\d", line)]
 
     @staticmethod
-    def _get_num(match: str) -> int:
-        if match in digit_mappings:
-            return digit_mappings[match]
-        return int(match)
-
-    def get_calibration_value_2(self, line: str) -> int:
-        """Search for first and last digits including spelled numbers."""
-
-        any_digit_pattern = "|".join([r"\d", *digit_mappings.keys()])
-        digits_matches = [
+    def find_all_digits(line: str) -> list[int]:
+        matches = (
             match.group()
             for idx in range(len(line))
-            if (match := re.match(any_digit_pattern, line[idx:])) is not None
-        ]
-        digits = [self._get_num(m) for m in digits_matches]
-        first, last = (digits[0], digits[-1])
-        return 10 * first + last
+            if (match := re.match(digit_pattern, line[idx:])) is not None
+        )
+        return [int(digit_mappings.get(match, match)) for match in matches]
+
+    @staticmethod
+    def get_calibration_value(digits: list[int]) -> int:
+        return 10 * digits[0] + digits[-1]
 
     def solve_part_1(self) -> int | None:
-        return sum(self.get_calibration_value(line) for line in self.lines)
+        return sum(
+            self.get_calibration_value(self.find_numeral_digits(line))
+            for line in self.lines
+        )
 
     def solve_part_2(self) -> int | None:
-        return sum(self.get_calibration_value_2(line) for line in self.lines)
+        return sum(
+            self.get_calibration_value(self.find_all_digits(line))
+            for line in self.lines
+        )
 
 
-if __name__ == "__main__":
-    input_ = input_file.read_text().strip()
+def main(
+    input_text: str = "",
+    input_file: Path = Path(__file__).parent / "input.txt",
+) -> None:
+    """Day 1: Trebuchet?!
+
+    https://adventofcode.com/2023/day/1
+    """
+    input_ = input_text or input_file.read_text().strip()
     solver = Solver(input_)
-
-    print("--- Day 1: Trebuchet?! ---")
-    print("https://adventofcode.com/2023/day/1")
 
     print("(1) What is the sum of all of the calibration values?")
     part_1 = solver.solve_part_1()
     print("Solution: " + str(part_1))
-    print("Expected: 53386")
 
     print("(2) What is the sum of all of the calibration values?")
     part_2 = solver.solve_part_2()
     print("Solution: " + str(part_2))
-    print("Expected: 53312")
+
+
+if __name__ == "__main__":
+    main()
